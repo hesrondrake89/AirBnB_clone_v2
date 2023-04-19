73c50f81d28c320e416faa7020cf9f0d2b360092
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-19 15:34:18 +0000

    tasks

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- this script prepares a MySQL server for the project
--- create project testing database with the name : hbnb_test_db
+-- create project testing database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
 -- with the password : hbnb_test_pwd if it dosen't exist
