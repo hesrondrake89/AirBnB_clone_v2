385a2e9c115839d7448f985f848d1333ffe4df72
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-10 10:17:44 +0000

    tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
+# Configures a web server to be used for deploying the web_static files.
 
-# Update package list and install nginx
-sudo apt-get update
-sudo apt-get -y install nginx
+apt-get update
+apt-get install -y nginx
 
-# Allow HTTP traffic through firewall
-sudo ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create directory structure for web_static
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create test HTML file
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Create symbolic link to current release
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Set ownership of web_static to ubuntu user
-sudo chown -R ubuntu:ubuntu /data/
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Update Nginx config to serve static content
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Restart Nginx service
-sudo service nginx restart
+service nginx restart
