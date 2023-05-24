9a9d970f2837f8db87a908e54d6954b40a71dff3
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-24 12:35:39 +0000

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Initiates a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
+The application is configured to listen on IP address 0.0.0.0 and port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
+    /: Displays "Hello HBNB!".
+    /hbnb: Displays "HBNB".
     /c/<text>: Displays 'C' followed by the value of <text>.
     /python/(<text>): Displays 'Python' followed by the value of <text>.
 """
@@ -47,4 +47,4 @@ def python(text="is cool"):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
