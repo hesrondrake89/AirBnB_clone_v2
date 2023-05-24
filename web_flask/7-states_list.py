9af4eb68ba305ea4c94fe97c53fae85a7637b79c
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-24 13:53:21 +0000

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,8 @@
 """Initiates a Flask web application.
 
 The application is configured to listen on IP address 0.0.0.0 and port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
 """
 from models import storage
 from flask import Flask
