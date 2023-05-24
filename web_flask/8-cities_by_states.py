29313597c3a57440959c60eb9838edf7abf08c75
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-24 12:47:31 +0000

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Initiates a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
+The application is configured to listen on IP address 0.0.0.0 and port 5000.
 """
 from models import storage
 from flask import Flask
@@ -14,19 +12,16 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
+    
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+   
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
