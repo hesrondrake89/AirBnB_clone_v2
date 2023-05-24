3c6b2fc3e3ecfc8bbcebbb63c007c2f914aa0d78
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-24 12:21:40 +0000

    tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays the message "Hello HBNB!" when accessed.
+"""
 from flask import Flask
 
 app = Flask(__name__)
