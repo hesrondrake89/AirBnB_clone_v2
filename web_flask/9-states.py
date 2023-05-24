92d801143da2252e999da85a99cc60ccee50623f
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-24 12:58:32 +0000

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Initiates a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
+The application is configured to listen on IP address 0.0.0.0 and port 5000.
 """
 from models import storage
 from flask import Flask
@@ -15,17 +12,14 @@ app = Flask(__name__)
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
+    
     states = storage.all("State")
     return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
+    
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
@@ -34,9 +28,9 @@ def states_id(id):
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
