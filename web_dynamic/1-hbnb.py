6a435535058c42e717ddaaba1af0f9c2612905d0
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-06-05 16:30:27 +0000

    tasks

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -6,28 +6,21 @@ from flask import Flask, render_template, url_for
 from models import storage
 import uuid
 
-# flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
-# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
+
     storage.close()
 
 
 @app.route('/1-hbnb')
 def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
+    
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
@@ -43,6 +36,5 @@ def hbnb_filters(the_id=None):
                            cache_id=cache_id)
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
+
     app.run(host=host, port=port)
