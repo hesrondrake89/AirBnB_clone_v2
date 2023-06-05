d2991e6ebeee6e2cee15486d382cd0465a22d18b
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-06-05 16:31:59 +0000

    Update 101-hbnb.py

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+This Flask app integrates an Airbnb static HTML template.
 """
 from flask import Flask, render_template, url_for
 from models import storage
 import uuid
 
 
-# flask setup
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
-# begin flask page rendering
+
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
+  
     storage.close()
 
 
 @app.route('/101-hbnb')
 def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
+    
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
@@ -43,6 +38,5 @@ def hbnb_filters(the_id=None):
                            cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
+
     app.run(host=host, port=port)
